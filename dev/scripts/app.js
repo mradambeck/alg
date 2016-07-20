7dd9155ccc2d70da6a69b92cbae6acfbc5441eb0
mradambeck <mr.adambeck@gmail.com>
2016-07-20 14:46:42 -0700

    autocomplete is functioning, but need to figure out way to handle categories

---
+++ dev/scripts/app.js
@@ -0,0 +1,41 @@
+console.log('sanity check: app.js is running.');
+
+var dedupe = function (array) {
+  return Array.from(new Set(array));
+};
+
+var generateKeywords = function (hits, typeToFilter) {
+
+  var newArray = [];
+
+  for (var i=0; i < hits.length; i++) {
+
+    switch(typeToFilter) {
+      case 'brand':
+        newArray.push(hits[i].brand);
+        break;
+      case 'name':
+        newArray.push(hits[i].name);
+        break;
+      case 'categories':
+        for(var j=0; j < hits[i].categories.length; j++) {
+
+          newArray.push(hits[i].categories[j]);
+          // var catSubString = hits[i].categories[j].substr(0, query.length);
+          //
+          // if ( catSubString === query) {
+          //   newArray.push(hits[i].categories[j]);
+          // }
+        }
+        break;
+      default:
+        console.error('no typeToFilter provided in generateKeywords()');
+
+    }
+
+  }
+  var keywordArray = dedupe(newArray).sort();
+
+  // console.log(newArray);
+  return keywordArray;
+};
--- dev/scripts/search.js
+++ dev/scripts/search.js
@@ -1,34 +1,99 @@
 console.log('Sanity check: search.js has been called');
 
-angular
-  .module('algolia-challenge', ['algoliasearch'])
-  .controller('SearchCtrl', ['$scope', 'algolia', function($scope, algolia) {
+var app = angular.module('algolia-challenge', ['algoliasearch']);
 
-    $scope.search = {
-      query: '',
-      hits: []
-    };
+app.controller('SearchCtrl', ['$scope', 'algolia', function($scope, algolia) {
 
-    // API access tokens
-    var appId = 'LSTLHX1M78',
-        apiKey = '2d33957a1e2b70a7d312983b2a1058d7',
-        searchIndexName = 'best_buy-demo_data';
+  $scope.search = {
+    query: '',
+    hits: [],
+    keywords: []
+  };
 
-    var client = algolia.Client(appId, apiKey);
-    var index = client.initIndex(searchIndexName);
 
-    $scope.$watch('search.query', function() {
+
+
+
+  // API access tokens
+  var appId = 'LSTLHX1M78',
+      apiKey = '2d33957a1e2b70a7d312983b2a1058d7';
+
+  // Setup Algolia client and indices
+  var client = algolia.Client(appId, apiKey),
+      searchIndex = client.initIndex('best_buy-demo_data'),
+      brandIndex = client.initIndex('best_buy-brand'),
+      nameIndex = client.initIndex('best_buy-name'),
+      categoriesIndex = client.initIndex('best_buy-categories');
+
+
+
+
+
+  $scope.$watch('search.query', function() {
+
+    // SEARCH RESULTS
+    searchIndex.search($scope.search.query)
+      .then(function searchSuccess(content) {
+        var hits = content.hits;
+
+        // add content of search results to scope for display in view
+        $scope.search.hits = hits;
+
+      }, function searchFailure(err) {
+        console.error(err);
+      }
+    );
+
+
+    // Generate keywords for Autocomplete
+    var createAutoComp = function (keyType, index) {
+      $scope.search.keywords = [];
       index.search($scope.search.query)
         .then(function searchSuccess(content) {
-          console.log(content);
-          // add content of search results to scope for display in view
-          $scope.search.hits = content.hits;
-        }, function searchFailure(err) {
-          console.log(err);
-      });
-    });
+            var thisKeyArray = generateKeywords(content.hits, keyType);
+            while (thisKeyArray.length > 0) {
+              $scope.search.keywords.push(thisKeyArray.pop());
+              $scope.search.keywords = dedupe($scope.search.keywords).sort().splice(0, 10);
 
-    $scope.setQuery = function(newQuery) {
-      $scope.search.query = newQuery;
+            }
+        }, function searchFailure(err) {
+          console.error(err);
+        }
+      );
     };
-  }]);
+
+    // var query = $scope.search.query;
+    // console.log('search.js - query = ', query);
+
+    createAutoComp('brand', brandIndex);
+    createAutoComp('name', nameIndex);
+    // createAutoComp('categories', categoriesIndex, query);
+
+
+
+  });
+
+  $scope.setQuery = function(newQuery) {
+    $scope.search.query = newQuery;
+  };
+}]);
+
+
+
+
+// only show unique items
+app.filter('unique', function() {
+  return function(collection, keyname) {
+    var output = [],
+        keys = [];
+
+    angular.forEach(collection, function(item) {
+      var key = item[keyname];
+      if(keys.indexOf(key) === -1) {
+        keys.push(key);
+        output.push(item);
+      }
+    });
+    return output;
+  };
+});
